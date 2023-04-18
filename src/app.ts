649f0766cdcac4a8199a924efa7cb0e085faea2a
cuongho <cuongseven789@gmail.com>
2023-04-18 16:21:13 +0700

    update new code

--- src/app.ts
+++ src/app.ts
@@ -91,7 +91,11 @@ app.use(function (req, res, next) {
     next()
 })
 
-app.use(cors())
+app.use(
+    cors({
+        origin: '*',
+    })
+)
 
 app.use(express.static(path.join(__dirname, '../dist-frontend')))
 
