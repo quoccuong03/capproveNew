486c161a981b46b7481d2f30e6fa166d4432eabd
cuongho <cuongseven789@gmail.com>
2023-04-18 16:18:22 +0700

    update template capporver

---
+++ tests/GitHelper.test.ts
@@ -0,0 +1,129 @@
+import GitHelper from '../src/utils/GitHelper'
+
+test('Testing  - sanitizeRepoPathHttps', () => {
+    expect(
+        GitHelper.sanitizeRepoPathHttps(
+            '  https://github.com/username/repository.git/ '
+        )
+    ).toBe('github.com/username/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathHttps from SSH', () => {
+    expect(
+        GitHelper.sanitizeRepoPathHttps(
+            ' git@github.com/username/repository.git/  '
+        )
+    ).toBe('github.com/username/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathSsh', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh(
+            ' git@github.com:username/repository.git/  '
+        ).repoPath
+    ).toBe('ssh://git@github.com:22/username/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathSsh - port', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh(
+            ' git@github.com:username/repository.git/  '
+        ).port
+    ).toBe(22)
+})
+
+test('Testing  - sanitizeRepoPathSsh - custom port', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh(
+            ' git@github.com:1234/username/repository.git/  '
+        ).port
+    ).toBe(1234)
+})
+
+test('Testing  - sanitizeRepoPathSsh from HTTPS', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh(
+            '  https://github.com/username/repository.git/ '
+        ).repoPath
+    ).toBe('ssh://git@github.com:22/username/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathSsh - not git suffix', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh('  github.com/owner/repository ').repoPath
+    ).toBe('ssh://git@github.com:22/owner/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathSsh - alt domain', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh(
+            '  git@git.alt-domain.com/owner/repository.git/ '
+        ).repoPath
+    ).toBe('ssh://git@git.alt-domain.com:22/owner/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathSsh - alt user', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh(
+            '  foobar@github.com/owner/repository.git/ '
+        ).repoPath
+    ).toBe('ssh://foobar@github.com:22/owner/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathSsh - default user', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh('  github.com/owner/repository.git/ ')
+            .repoPath
+    ).toBe('ssh://git@github.com:22/owner/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathSsh - no owner', () => {
+    expect(
+        GitHelper.sanitizeRepoPathSsh('  git@github.com:repository.git/ ')
+            .repoPath
+    ).toBe('ssh://git@github.com:22/repository.git')
+})
+
+test('Testing  - sanitizeRepoPathSsh - invalid url', () => {
+    expect(() =>
+        GitHelper.sanitizeRepoPathSsh(
+            '  git:password@github.com/owner/repository.git/ '
+        )
+    ).toThrow(Error)
+})
+
+test('Testing  - getDomainFromSanitizedSshRepoPath - pure', () => {
+    expect(
+        GitHelper.getDomainFromSanitizedSshRepoPath(
+            'ssh://git@github.com:132/username/repository.git'
+        )
+    ).toBe('github.com')
+})
+
+test('Testing  - getDomainFromSanitizedSshRepoPath', () => {
+    expect(
+        GitHelper.getDomainFromSanitizedSshRepoPath(
+            GitHelper.sanitizeRepoPathSsh(
+                ' git@github.com:username/repository.git/  '
+            ).repoPath
+        )
+    ).toBe('github.com')
+})
+
+test('Testing  - getDomainFromSanitizedSshRepoPath from HTTPS', () => {
+    expect(
+        GitHelper.getDomainFromSanitizedSshRepoPath(
+            GitHelper.sanitizeRepoPathSsh(
+                '  https://github.com/username/repository.git/ '
+            ).repoPath
+        )
+    ).toBe('github.com')
+})
+
+test('Testing  - getDomainFromSanitizedSshRepoPath - alt domain', () => {
+    expect(
+        GitHelper.getDomainFromSanitizedSshRepoPath(
+            ' ssh://user@some.other-domain.com/owner/repository.git/ '
+        )
+    ).toBe('some.other-domain.com')
+})
